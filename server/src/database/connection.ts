6bc03173a37a04fe59ec099dd0d0ec9da3055e38
thiagoualace <ualacenp@gmail.com>
2020-06-05 21:14:32 -0300

    App

---
+++ server/src/database/connection.ts
@@ -0,0 +1,16 @@
+import knex from 'knex'; 
+import path from 'path'; //uni caminhos
+
+const connection = knex({
+    client: 'sqlite3',
+    connection: {
+        filename: path.resolve(__dirname, 'database.sqlite'),
+    },
+    useNullAsDefault: true,
+});
+
+export default connection;
+
+
+
+
